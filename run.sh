df4bb585033aa73da736ab1564219515375420cf
Nicholas Dronen <ndronen@gmail.com>
2018-06-07 11:11:42 -0600

    Prepare run.sh to work in ML Platform

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
-#source activate tensorflow
+input_dir=$1
+output_dir=$2
+params=$3
 
-model=PSPNet-Res50
+model_name=$(echo $params | json model-name)
 
-python main.py --mode train --dataset HT --num_val_images 498 --h_flip True --model $model | tee $model-Train.txt
-python main.py --mode val --dataset HT --model $model 2>&1 | tee $model-Val.txt
-python main.py --mode test --dataset HT --model $model 2>&1 | tee $model-Test.txt
+python main.py --mode train --dataset HT --num_val_images 498 --h_flip True --model ${model_name} | tee ${model_name}-Train.txt
+python main.py --mode val --dataset HT --model ${model_name} 2>&1 | tee ${model_name}-Val.txt
+python main.py --mode test --dataset HT --model ${model_name} 2>&1 | tee ${model_name}-Test.txt
