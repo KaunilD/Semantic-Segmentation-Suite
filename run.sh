c8ed6700355995ab89876e21b98ce96bc01d9ed7
Nicholas Dronen <ndronen@gmail.com>
2018-06-07 15:54:34 -0600

    Prep data without creating a symbolic link

--- run.sh
+++ run.sh
@@ -6,11 +6,15 @@ params=$3
 
 # Prep the data.
 dataset_name=$(echo $params | json dataset-name)
-ln -s $input_dir input
-cd ${dataset_name} && ./prep_data.py
+cd ${dataset_name}
+sed -i "s,INPUT_DIR,$input_dir,g" train.txt
+sed -i "s,INPUT_DIR,$input_dir,g" val.txt
+sed -i "s,INPUT_DIR,$input_dir,g" test.txt
+./prep_data.py
+cd ..
 
 # Train the model.
 model_name=$(echo $params | json model-name)
-python main.py --mode train --dataset HT --num_val_images 498 --h_flip True --model ${model_name} | tee ${model_name}-Train.txt
-python main.py --mode val --dataset HT --model ${model_name} 2>&1 | tee ${model_name}-Val.txt
-python main.py --mode test --dataset HT --model ${model_name} 2>&1 | tee ${model_name}-Test.txt
+python main.py --mode train --dataset ${dataset_name} --num_val_images 498 --h_flip True --model ${model_name} | tee ${model_name}-Train.txt
+python main.py --mode val --dataset ${dataset_name} --model ${model_name} 2>&1 | tee ${model_name}-Val.txt
+python main.py --mode test --dataset ${dataset_name} --model ${model_name} 2>&1 | tee ${model_name}-Test.txt
