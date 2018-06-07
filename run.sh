5313761fa903d8c303345a56426462dcb93db19f
Nicholas Dronen <ndronen@gmail.com>
2018-06-07 11:15:05 -0600

    Add dataset-prepping code to run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,13 @@ input_dir=$1
 output_dir=$2
 params=$3
 
-model_name=$(echo $params | json model-name)
+# Prep the data.
+dataset_name=$(echo $params | json dataset-name)
+ln -s $input_dir input
+cd ${dataset_name} && ./prep_data.py
 
+# Train the model.
+model_name=$(echo $params | json model-name)
 python main.py --mode train --dataset HT --num_val_images 498 --h_flip True --model ${model_name} | tee ${model_name}-Train.txt
 python main.py --mode val --dataset HT --model ${model_name} 2>&1 | tee ${model_name}-Val.txt
 python main.py --mode test --dataset HT --model ${model_name} 2>&1 | tee ${model_name}-Test.txt
