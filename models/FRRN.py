6a65d01d04e803ca1e31708b5381c3b7c2034535
Nicholas Dronen <ndronen@gmail.com>
2018-06-12 16:16:11 -0600

    Refactor FRNN models to work with rectangular images

--- models/FRRN.py
+++ models/FRRN.py
@@ -1,12 +1,31 @@
+import numbers
+
 import tensorflow as tf
 from tensorflow.contrib import slim
 import resnet_v1
 
-def Upsampling(inputs,scale):
-    return tf.image.resize_nearest_neighbor(inputs, size=[tf.shape(inputs)[1]*scale,  tf.shape(inputs)[2]*scale])
 
-def Unpooling(inputs,scale):
-    return tf.image.resize_bilinear(inputs, size=[tf.shape(inputs)[1]*scale,  tf.shape(inputs)[2]*scale])
+def Upsampling(inputs, size=None, target_tensor=None):
+    assert (size is not None or target_tensor is not None)
+    assert ((size is not None) != (target_tensor is not None))
+    if target_tensor is not None:
+        size = [tf.shape(target_tensor)[1], tf.shape(target_tensor)[2]]
+    elif isinstance(size, numbers.Integral):
+        size = [tf.shape(inputs)[1]*size, tf.shape(inputs)[2]*size]
+    result = tf.image.resize_nearest_neighbor(inputs, size=size)
+    return result
+
+
+def Unpooling(inputs, size=None, target_tensor=None):
+    assert (size is not None or target_tensor is not None)
+    assert ((size is not None) != (target_tensor is not None))
+    if target_tensor is not None:
+        size = [tf.shape(target_tensor)[1], tf.shape(target_tensor)[2]]
+    elif isinstance(size, numbers.Integral):
+        size = [tf.shape(inputs)[1]*size, tf.shape(inputs)[2]*size]
+    result = tf.image.resize_bilinear(inputs, size=size)
+    return result
+
 
 def ResidualUnit(inputs, n_filters=48, filter_size=3):
     """
@@ -29,6 +48,7 @@ def ResidualUnit(inputs, n_filters=48, filter_size=3):
 
     return net
 
+
 def FullResolutionResidualUnit(pool_stream, res_stream, n_filters_3, n_filters_1, pool_scale):
     """
     A full resolution residual unit
@@ -44,9 +64,8 @@ def FullResolutionResidualUnit(pool_stream, res_stream, n_filters_3, n_filters_1
       Output of full resolution residual block
     """
 
-    G = tf.concat([pool_stream, slim.pool(res_stream, [pool_scale, pool_scale], stride=[pool_scale, pool_scale], pooling_type='MAX')], axis=-1)
-
-    
+    res_stream_pooled = slim.pool(res_stream, [pool_scale, pool_scale], stride=[pool_scale, pool_scale], pooling_type='MAX')
+    G = tf.concat([pool_stream, res_stream_pooled], axis=-1)
 
     net = slim.conv2d(G, n_filters_3, kernel_size=3, activation_fn=None)
     net = slim.batch_norm(net, fused=True)
@@ -56,7 +75,7 @@ def FullResolutionResidualUnit(pool_stream, res_stream, n_filters_3, n_filters_1
     pool_stream_out = tf.nn.relu(net)
 
     net = slim.conv2d(pool_stream_out, n_filters_1, kernel_size=1, activation_fn=None)
-    net = Upsampling(net, scale=pool_scale)
+    net = Upsampling(net, target_tensor=res_stream)
     res_stream_out = tf.add(res_stream, net)
 
     return pool_stream_out, res_stream_out
@@ -93,6 +112,7 @@ def build_frrn(inputs, num_classes, preset_model='FRRN-A'):
         #####################
         # Downsampling Path 
         #####################
+        pool_stream1 = net
         pool_stream = slim.pool(net, [2, 2], stride=[2, 2], pooling_type='MAX')
         res_stream = slim.conv2d(net, 32, kernel_size=1, activation_fn=None)
 
@@ -100,16 +120,19 @@ def build_frrn(inputs, num_classes, preset_model='FRRN-A'):
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
 
+        pool_stream2 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX') 
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
 
+        pool_stream3 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX')
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=8)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=8)
 
+        pool_stream4 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX')
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=16)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=16)
@@ -117,24 +140,25 @@ def build_frrn(inputs, num_classes, preset_model='FRRN-A'):
         #####################
         # Upsampling Path 
         #####################
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=8)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=8)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream3)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream2)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream1)
 
         #####################
         # Final Stage 
         #####################
         net = tf.concat([pool_stream, res_stream], axis=-1)
+
         net = ResidualUnit(net, n_filters=48, filter_size=3)
         net = ResidualUnit(net, n_filters=48, filter_size=3)
         net = ResidualUnit(net, n_filters=48, filter_size=3)
@@ -159,6 +183,7 @@ def build_frrn(inputs, num_classes, preset_model='FRRN-A'):
         #####################
         # Downsampling Path 
         #####################
+        pool_stream1 = net
         pool_stream = slim.pool(net, [2, 2], stride=[2, 2], pooling_type='MAX')
         res_stream = slim.conv2d(net, 32, kernel_size=1, activation_fn=None)
 
@@ -166,20 +191,24 @@ def build_frrn(inputs, num_classes, preset_model='FRRN-A'):
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
 
+        pool_stream2 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX') 
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
 
+        pool_stream3 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX')
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=8)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=8)
 
+        pool_stream4 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX')
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=16)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=16)
 
+        pool_stream5 = pool_stream
         pool_stream = slim.pool(pool_stream, [2, 2], stride=[2, 2], pooling_type='MAX')
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=32)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=384, n_filters_1=32, pool_scale=32)
@@ -187,33 +216,35 @@ def build_frrn(inputs, num_classes, preset_model='FRRN-A'):
         #####################
         # Upsampling Path 
         #####################
-        pool_stream = Unpooling(pool_stream, 2)
-        pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=17)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream5)
+        pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=16)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=16)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=8)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=8)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream3)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=192, n_filters_1=32, pool_scale=4)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream2)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
         pool_stream, res_stream = FullResolutionResidualUnit(pool_stream=pool_stream, res_stream=res_stream, n_filters_3=96, n_filters_1=32, pool_scale=2)
 
-        pool_stream = Unpooling(pool_stream, 2)
+        pool_stream = Unpooling(pool_stream, target_tensor=pool_stream1)
 
         #####################
         # Final Stage 
         #####################
         net = tf.concat([pool_stream, res_stream], axis=-1)
+
         net = ResidualUnit(net, n_filters=48, filter_size=3)
         net = ResidualUnit(net, n_filters=48, filter_size=3)
         net = ResidualUnit(net, n_filters=48, filter_size=3)
 
         net = slim.conv2d(net, num_classes, [1, 1], activation_fn=None, scope='logits')
+
         return net
 
     else:
