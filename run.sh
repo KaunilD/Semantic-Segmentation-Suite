9ebb4adeec57de418d3ae1f9f05374a6c34cd7b8
Nicholas Dronen <ndronen@gmail.com>
2018-06-21 13:07:17 -0600

    Handle `continue-training`, `batch-size`, `optimizer`, and `checkpoint-dir-uri` in run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,15 @@ then
 fi
 
 model_name=$(echo $params | json model-name)
+checkpoint_dir_uri=$(echo $params | json checkpoint-dir-uri)
+
+if [[ ! -z $checkpoint_dir_uri ]]
+then
+    if [ ! -d checkpoints ]
+    then
+        aws s3 sync $checkpoint_dir_uri checkpoints
+    fi
+fi
 
 function update_extra_args {
     params=$1
@@ -30,6 +39,9 @@ extra_args=""
 extra_args=$(update_extra_args "$params" "$extra_args" class-balancing)
 extra_args=$(update_extra_args "$params" "$extra_args" loss-func)
 extra_args=$(update_extra_args "$params" "$extra_args" learning-rate)
+extra_args=$(update_extra_args "$params" "$extra_args" continue-training)
+extra_args=$(update_extra_args "$params" "$extra_args" batch-size)
+extra_args=$(update_extra_args "$params" "$extra_args" optimizer)
 
 python main.py --mode train --dataset ${dataset_name} --h_flip True --model ${model_name} $extra_args --crop_height 360 --crop_width 480 | tee ${model_name}-Train.txt
 python main.py --mode val --dataset ${dataset_name} --model ${model_name} 2>&1 | tee ${model_name}-Val.txt
